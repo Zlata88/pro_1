845303bcdb8deede96f41425e7437b5171087884
Zlata88 <Zlatablumen8@yandex.ru>
2023-08-22 12:36:03 +0300

    Не совсем поняла вторую часть задания. Не получается ничего...

--- adverisements/App/models.py
+++ adverisements/App/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
+class Adverisements(models.Model):
+    # название - title
+    # описание -description
+    # цена - price
+    # торг - auction
+    # создано - created
+    # изменено - updated
+    id = models.CharField("id", max_length=64, primary_key=True)
+
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена'
+    )
+    auction = models.BooleanField(
+        default=False,
+        verbose_name='Торг'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата обновлния'
+    )
+
+    def __str__(self):
+        return f"Adverisements: Adverisements(id={self.id}, title={self.title}, price={self.price})"
+
+
+class Meta:
+    db_table = 'advertisements'
--- adverisements/adverisements/settings.py
+++ adverisements/adverisements/settings.py
@@ -77,6 +77,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dbname',
+#         'USER': 'username',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
